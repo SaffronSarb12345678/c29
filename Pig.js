f04f799c050afd8449ddee596a9a2a5bb62737f6
SaffronSarb12345678 <75558263+SaffronSarb12345678@users.noreply.github.com>
2021-04-06 16:29:33 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,14 @@
+class Pig extends parent{
+
+    constructor(x,y,width,height){
+      
+        super(x,y,width,height);
+        this.image=loadImage("sprites/enemy.png")
+        
+    
+    
+    }
+    
+    
+    
+    }
